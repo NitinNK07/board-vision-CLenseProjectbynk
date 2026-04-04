a3dc0dd9c6291a4e9a4bd025605ba7be5e67b30b
Nitin NK <nitinkolhe2004@gmail.com>
2026-04-04 13:33:35 +0530

    Remove spaces from DATABASE_URL in entrypoint.sh

--- board-vision-app/entrypoint.sh
+++ board-vision-app/entrypoint.sh
@@ -2,6 +2,8 @@
 echo "Starting application with database URL fix..."
 echo "DATABASE_URL is set: $(echo $DATABASE_URL | cut -c1-20)..."
 
-# Use Java system property to override spring.datasource.url
-# This adds jdbc: prefix to Render's postgresql:// URL
-exec java -Dspring.datasource.url="jdbc:${DATABASE_URL}" -jar app.jar "$@"
+# Remove any spaces from DATABASE_URL (Render sometimes adds them)
+CLEAN_URL=$(echo "$DATABASE_URL" | tr -d ' ')
+
+# Use Java system property to override spring.datasource.url with jdbc: prefix
+exec java -Dspring.datasource.url="jdbc:${CLEAN_URL}" -jar app.jar "$@"
